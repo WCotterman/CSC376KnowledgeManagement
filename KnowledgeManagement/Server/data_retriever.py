118c730078be1fdafa0400bb82f38b1a47adf461
WCotterman <wcotterman@gmail.com>
2016-11-05 06:43:48 -0500

    Finished Authentication merge
    -Added functions from the deleted Server.py file to data_retriever.py.
    -Updated the new functions parameters/comments to comply with semantics

--- KnowledgeManagement/Server/data_retriever.py
+++ KnowledgeManagement/Server/data_retriever.py
@@ -2,6 +2,7 @@
 
 import threading
 import json
+import sys,socket,datetime,sqlite3,random
 from connections import connections
 
 class DataRetriever( threading.Thread ):
@@ -46,15 +47,52 @@ class DataRetriever( threading.Thread ):
         else:
             return "NO"
 
-    def upload(self, fileName, file):
+    def upload(self, fileName, source, file, category=None, keys=None):
         """
         Inserts the new file into the db
 
         :param fileName: name of new file
+        :param source: filepath of the new file
         :param file: contents of new file
+        :param category(optional): file category, for top level organization
+        :param keys(optional): keywords for search functions
 
         :return: a msg indicating success or not
         """
+
+        #create new empty file
+        f = open(fileName,'wb') #open and write to binary file
+
+        ##PRINT DATABASE ENTRY INFO#####
+        #get uid
+        uid = random.random()*10
+
+        ####GET FILE FROM TRANSFER ######
+        #get start of file
+        flag = sc.recv(3).decode() #start of file 3 byte flag
+        if(flag  == "SOF"):
+            print("Start of file transfer")
+            #get file data
+            data = sc.recv(1024)
+            while (data):
+                #print (data)
+                #write recieved data to file
+                f.write(data)
+                #receive next 1024 bytes
+                data = sc.recv(1024)
+                print("packet received")
+        print("End of File Transfer..")
+        #close file
+        f.close()
+
+        #CREATE DATABASE IF NON EXISTANT
+        createDatabase()
+        #ADD FILE TO DATABASE
+        addToDatabase(int(uid),address,fn,cat,keys)
+        #PRINT DATABASE ENTRIES
+        get_posts()
+
+    #todo: return success token
 
     def search(self, fileName):
         """
@@ -74,3 +112,37 @@ class DataRetriever( threading.Thread ):
 
         :return: a msg indicating success or not
         """
+
+    def createDatabase(self):
+        """Create the database if it doesn't already exist"""
+        conn = sqlite3.connect('files.db')
+        c = conn.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS Files(ID TEXT, source TEXT, filename TEXT, category TEXT, keywords TEXT, timestamp TEXT)")
+
+    def addToDatabase(self, uid, source, filename, category=None, keys=None):
+        """
+    Add file entry to database
+
+        :param uid: unique identifier for file
+    :param source: filepath of the new file
+    :param filename: name of new file
+    :param category(optional): file category, for top level organization
+    :param keys(optional): keywords for search functions
+    """
+        timestamp = datetime.datetime.now()
+        conn = sqlite3.connect('files.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO Files VALUES (?, ?, ?, ?, ?, ?)", (int(uid),str(source),str(filename),str(category),str(keys),str(timestamp)))
+        conn.commit()
+        print("Item added to database")
+
+    #todo: return success token
+
+    def get_posts(self):
+        """Print all database entries"""
+        conn = sqlite3.connect('files.db')
+        c = conn.cursor()
+        c.execute("SELECT ID, source, filename, category, keywords, timestamp  from Files")
+        conn.commit()
+        for row in c:
+            print(row)
